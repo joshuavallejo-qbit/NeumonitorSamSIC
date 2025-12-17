f3d9e8fd6a740915e74e5eae5df89f2136375aec
Joshúa Castillo <castle2004josh2@gmail.com>
2025-12-17 16:23:11 -0500

    Update page.tsx
    Añadiendo logo

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,5 +1,18 @@
 import React from 'react';
 
 export default function Page() {
-  return <div>Bienvenido a NeuroMonitor</div>;
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+     
+      {/* Imagen principal */}
+      <img 
+        src="/logo.jpg" 
+        alt="Imagen principal" 
+        style={{ width: '300px', marginBottom: '20px' }} 
+      />
+
+      {/* Texto de bienvenida */}
+      <h1>Bienvenido a NeuroMonitor</h1>
+    </div>
+  );
 }
